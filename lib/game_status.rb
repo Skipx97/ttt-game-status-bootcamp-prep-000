0713f154990feb85da4b88741788cb52b685c644
Derrick Skipwith <dskipwith15@gmail.com>
2018-07-13 21:23:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,30 +6,57 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 def won?(board)
-  WIN_COMBINATIONS.find do |do|
-    board[sub[0]] == board[sub[1]] && board[sub[1]] == board[sub[2]] && position_taken?(board,sub[0])
+  winning_array = []
+  WIN_COMBINATIONS.each do |wincombo|
+    if board[wincombo[0]] == "X" && board[wincombo[1]] == "X" && board[wincombo[2]] == "X"
+      return wincombo
+    elsif board[wincombo[0]] == "O" && board[wincombo[1]] == "O" && board[wincombo[2]] == "O"
+      return wincombo
+    else
+      winning_array = nil
+    end
   end
+  return winning_array
 end
+
 def full?(board)
-  !board.any? {|x| x == ""}
+  full_board = board.none? do |index|
+    index == " "
+  end
+  return full_board
 end
 
 def draw?(board)
-  !won?(board) && full?(board)
+  if won?(board) != nil
+    return FALSE
+  elsif full?(board) == TRUE
+    return TRUE
+  else
+    return FALSE
+  end
 end
 
 def over?(board)
-  if !won?(board) && !full?(board)
-    false
+  if won?(board) != nil
+    return TRUE
+  elsif draw?(board) == TRUE
+    return TRUE
   else
-    true
+    return FALSE
   end
 end
 
+
 def winner(board)
-  if winner = won?(board)
-    board[winner[0]]
-  else
-    nil
+  WIN_COMBINATIONS.each do |wincombo|
+    if board[wincombo[0]] == "X" && board[wincombo[1]] == "X" && board[wincombo[2]] == "X"
+      return "X"
+    elsif board[wincombo[0]] == "O" && board[wincombo[1]] == "O" && board[wincombo[2]] == "O"
+      return "O"
+    else
+    
+    end
   end
+  return nil
 end
+
